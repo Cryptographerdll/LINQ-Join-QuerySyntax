26e11adc51af0b3634aa1b12dce4e778012cb12d
Cryptography Enginner <85350190+khalilDup@users.noreply.github.com>
2021-12-14 15:11:41 +0100

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ_Join_QuerySyntax
+{
+    public class Student
+    {
+        public string Name { get; set; }
+    }
+}
